e97225059a0815342ee241464b47d82743ba4d91
lihaorun <954645392@qq.com>
2024-02-23 22:20:34 +0800

    webpack base

--- docs/.vitepress/sidebar/enginee/webpack.ts
+++ docs/.vitepress/sidebar/enginee/webpack.ts
@@ -9,6 +9,14 @@ const webpackSide = {
       text: '基本配置',
       link: '/package/webpack/基本配置',
     },
+    {
+      text: '多入口打包',
+      link: '/package/webpack/多入口打包',
+    },
+    {
+      text: '代码抽离',
+      link: '/package/webpack/代码抽离',
+    },
   ],
 } as DefaultTheme.SidebarItem
 
